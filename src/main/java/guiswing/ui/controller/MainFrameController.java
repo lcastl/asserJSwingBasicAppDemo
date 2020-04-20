ae187527de8daa896678c73dafc3e1882b1f5954
Juan Montes <juan.montes@globant.com>
2020-04-20 16:31:11 -0500

    change maven to gradle

--- src/main/java/guiswing/ui/controller/MainFrameController.java
+++ src/main/java/guiswing/ui/controller/MainFrameController.java
@@ -2,8 +2,8 @@ package guiswing.ui.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
+import javax.swing.*;
+
 import guiswing.ui.view.MainFrame;
 
 public class MainFrameController {
